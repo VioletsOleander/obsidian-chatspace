ad89be3f29aab7794b874f5ff2ac5a86ad2a9b21
Xinjie Chen <1377232072@qq.com>
2026-05-15 22:32:59 +0800

    chore: configure esbuild and eslint (#7)

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import obsidianmd from "eslint-plugin-obsidianmd";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const config = defineConfig([
+  {
+    files: ["src/**/*.ts"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      globals: { ...globals.browser },
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  ...obsidianmd.configs.recommended,
+  globalIgnores([
+    "dist/",
+    "main.js",
+    "build.js",
+    "eslint.config.js",
+  ]),
+]);
+
+export default config;
