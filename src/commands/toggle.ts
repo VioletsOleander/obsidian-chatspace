1b6245beb76108080cd1ebab5e691d40aee79560
VioletsOleander <1377232072@qq.com>
2026-09-06 15:49:33 +0800

    fix: hotkey not activated after 'toggle-chat'
    It is assumed that the view's scope will only be activated after focus transfer. Therefore we need
    to call `workspace.setActiveLeaf` instead of `workspace.revealLeaf`.

--- src/commands/toggle.ts
+++ src/commands/toggle.ts
@@ -18,7 +18,7 @@ async function activateView(plugin: ChatSpace, viewType: string): Promise<void> 
     leaf = firstLeaf;
   }
 
-  return workspace.revealLeaf(leaf);
+  return workspace.setActiveLeaf(leaf, { focus: true });
 }
 
 /** Reveal existing chat view or create a new chat view. */
